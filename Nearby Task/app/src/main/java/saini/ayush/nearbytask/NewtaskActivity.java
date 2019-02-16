1b453eb4ebfa7852bacf3f423e02c0f311db9e7a
unknown <gketan2@gmail.com>
2019-02-17 02:15:33 +0530

    map added in newTaskActivity

--- Nearby Task/app/src/main/java/saini/ayush/nearbytask/NewtaskActivity.java
+++ Nearby Task/app/src/main/java/saini/ayush/nearbytask/NewtaskActivity.java
@@ -7,14 +7,22 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+
 import saini.ayush.nearbytask.model.DatabaseHelper;
 
-public class NewtaskActivity extends AppCompatActivity {
+public class NewtaskActivity extends AppCompatActivity implements OnMapReadyCallback {
     private DatabaseHelper db;
     private EditText newTask;
     private EditText titleView;
     private  String Title;
     private  String Task;
+    MapView mapView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +30,21 @@ public class NewtaskActivity extends AppCompatActivity {
         db = new DatabaseHelper(this);
         newTask = (EditText) findViewById(R.id.taskdetail);
         titleView = (EditText) findViewById(R.id.title);
+        mapView = findViewById (R.id.mapView);
+
+        initGoogleMap(savedInstanceState);
     }
+
+    public void initGoogleMap(Bundle savedInstanceState){
+        Bundle mapViewBundle = null;
+        if(savedInstanceState !=null){
+            mapViewBundle = savedInstanceState.getBundle ("MapViewBundleKey");
+        }
+        mapView.onCreate (mapViewBundle);
+        mapView.getMapAsync (this);
+    }
+
+
     public void saveTask(View view){
         Title= titleView.getText().toString();
         Task = newTask.getText().toString();
@@ -44,5 +66,50 @@ public class NewtaskActivity extends AppCompatActivity {
         super.onBackPressed ();
     }
 
+                       ////     GOOGLE API KEY INCLUDED      //////
 
+    @Override
+    public void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState (outState);
+        Bundle mapViewBundle = outState.getBundle ("MapViewBundleKey");
+        if(mapViewBundle == null){
+            mapViewBundle = new Bundle();
+            outState.putBundle ("MapViewBundleKey",mapViewBundle);
+        }
+        mapView.onSaveInstanceState (mapViewBundle);
+
+    }
+
+    @Override
+    public void onMapReady ( GoogleMap googleMap ) {
+        LatLng coordinate = new LatLng (26.249819, 78.169722);
+        CameraUpdate yourLocation = CameraUpdateFactory.newLatLngZoom(coordinate, 11);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(coordinate));
+        googleMap.animateCamera(yourLocation);
+    }
+
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
 }
